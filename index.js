79432855da737ba5f4e38c8191d7d794ec3b361e
Lalit <lalit.pratap.naidu@gmail.com>
2021-11-05 01:34:30 -0400

    1. Added routes 2. Modified routes in endpoint calls

--- index.js
+++ index.js
@@ -2,35 +2,13 @@ const express = require("express");
 const app = express();
 const port = 3000;
 
-const { createUser, fetchUser } = require("./business/index");
-
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-app.get("/api", (req, res) => {
-  res.send("Hello, world!");
-});
+// Middleware for handling routes
+app.use("/api/users", require("./routes/users"));
 
-app.get("/api/:userId", async (req, res) => {
-  try {
-    const id = req.params.userId;
-    const response = await fetchUser(id);
-    res.status(response.status).send(response.document);
-  } catch (err) {
-    res.status(err.status).send(err.message);
-  }
-});
-
-app.post("/api", async (req, res) => {
-  const body = req.body;
-  try {
-    const response = await createUser(body);
-    res.status(response.status).send(response.message);
-  } catch (err) {
-    res.status(err.status).send(err.message);
-  }
-});
-
+// Port
 app.listen(port, () => {
   console.log(`Listening on ${port}`);
 });
