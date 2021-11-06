2094164758b940a56170e9a002fe523330664a3d
Lalit <lalit.pratap.naidu@gmail.com>
2021-11-06 13:46:40 -0400

    1. Added jest config for unit test cases 2. Added files to gitignore 3. Added jest and jest-sonar-reporter 4. Added unit test cases for createUser

--- tests/unit/createUser/createUser.test.js
+++ tests/unit/createUser/createUser.test.js
@@ -1,6 +1,8 @@
 const { validateBody } = require("../../../helpers/validations");
+const { insertDB, fetchDocument } = require("../../../helpers/db");
 const statusConstants = require("../../../common/statusConstants");
 const errorMessages = require("../../../common/errorMessages");
+const { v4 } = require("uuid");
 
 describe("testing validateBody", () => {
   it("should return an error if firstName is not present in body", () => {
@@ -29,6 +31,15 @@ describe("testing validateBody", () => {
     }
   });
 
+  it("should return an error if both firstName and lastName is not present in body", () => {
+    try {
+      validateBody({});
+    } catch (err) {
+      expect(err.message).toBe(errorMessages.createUser.name);
+      expect(err.status).toBe(statusConstants.error.bad_request);
+    }
+  });
+
   it("should successfully pass validation if both firstName and lastName are present in body", () => {
     const body = {
       firstName: "firstName",
@@ -39,5 +50,25 @@ describe("testing validateBody", () => {
     try {
       validateBody(body);
     } catch (err) {}
+  });
+});
+
+describe("testing insertDB", () => {
+  it("should return an error if firstName is not present in body", async () => {
+    const body = {
+      userId: v4(),
+      firstName: "firstName",
+      lastName: "lastName",
+    };
+
+    try {
+      await insertDB(body);
+      const document = await fetchDocument({ userId: body.userId });
+      expect(document.userId).toBe(body.userId);
+      expect(document.firstName).toBe(body.firstName);
+      expect(document.lastName).toBe(body.lastName);
+    } catch (err) {
+      console.log("Mongo error test: ", err);
+    }
   });
 });
